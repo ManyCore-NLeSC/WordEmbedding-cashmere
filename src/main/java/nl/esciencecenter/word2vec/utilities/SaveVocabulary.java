0fc78504e31c5fc509f82cbf7ae6249ca1cdea92
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-08-31 15:36:48 +0200

    First implementation, utility to save a learned vocabulary.

---
+++ src/main/java/nl/esciencecenter/word2vec/utilities/SaveVocabulary.java
@@ -0,0 +1,17 @@
+package nl.esciencecenter.word2vec.utilities;
+
+import nl.esciencecenter.word2vec.data.Vocabulary;
+import nl.esciencecenter.word2vec.data.Word;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class SaveVocabulary {
+
+    public void save(BufferedWriter fileWriter, Vocabulary vocabulary) throws IOException {
+        for ( Word word : vocabulary.getWords() ) {
+            fileWriter.write(word.getWord() + " " + String.valueOf(word.getOccurrences()));
+            fileWriter.newLine();
+        }
+    }
+}
