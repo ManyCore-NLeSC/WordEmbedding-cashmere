f09f8f0184740b38653a9817c00877d991690178
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-08-31 15:34:32 +0200

    Class to represent a Vocabulary, i.e. a collection of words.

---
+++ src/main/java/nl/esciencecenter/word2vec/data/Vocabulary.java
@@ -0,0 +1,59 @@
+package nl.esciencecenter.word2vec.data;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Vocabulary {
+    private Integer occurrenceThreshold;
+    private HashMap<String, Word> words;
+
+    public Vocabulary() {
+        occurrenceThreshold = 5;
+        words = new HashMap<>();
+    }
+
+    public Vocabulary(Integer occurrenceThreshold) {
+        this.occurrenceThreshold = occurrenceThreshold;
+        words = new HashMap<>();
+    }
+
+    public void setOccurrenceThreshold(Integer occurrenceThreshold) {
+        this.occurrenceThreshold = occurrenceThreshold;
+    }
+
+    public void addWord(Word word) {
+        if ( words.containsKey(word.getWord()) ) {
+            words.get(word.getWord()).incrementOccurrences();
+        } else {
+            words.put(word.getWord(), word);
+            word.incrementOccurrences();
+        }
+    }
+
+    private void removeWord(String word) {
+        words.remove(word);
+    }
+
+    public Word getWord(String word) {
+        return words.get(word);
+    }
+
+    public Collection<Word> getWords() {
+        return words.values();
+    }
+
+    public Integer getNrWords() {
+        return words.size();
+    }
+
+    public void reduce() {
+        for ( Word word : words.values() ) {
+            if ( word.getOccurrences() <= occurrenceThreshold ) {
+                removeWord(word.getWord());
+            }
+        }
+        occurrenceThreshold++;
+    }
+}
