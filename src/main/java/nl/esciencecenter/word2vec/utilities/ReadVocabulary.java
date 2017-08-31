a7397604f3743452502ec68de4126ad7c17fc6e2
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-08-31 15:36:32 +0200

    First implementation, utility to read a previously learned vocabulary.

---
+++ src/main/java/nl/esciencecenter/word2vec/utilities/ReadVocabulary.java
@@ -0,0 +1,19 @@
+package nl.esciencecenter.word2vec.utilities;
+
+import nl.esciencecenter.word2vec.data.Vocabulary;
+import nl.esciencecenter.word2vec.data.Word;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ReadVocabulary {
+
+    public void read(BufferedReader fileReader, Vocabulary vocabulary) throws IOException {
+        String line;
+
+        while ( (line = fileReader.readLine()) != null ) {
+            String [] values = line.split("[ ]");
+            vocabulary.addWord(new Word(values[0], Integer.parseInt(values[1])));
+        }
+    }
+}
