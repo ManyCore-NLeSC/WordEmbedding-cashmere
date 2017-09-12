ca557262f4f85cf0e457f5029b2cd2e5265a9c6e
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-09-12 14:21:00 +0200

    Removed unused import.

--- src/main/java/nl/esciencecenter/word2vec/utilities/LearnVocabulary.java
+++ src/main/java/nl/esciencecenter/word2vec/utilities/LearnVocabulary.java
@@ -1,7 +1,6 @@
 package nl.esciencecenter.word2vec.utilities;
 
 import nl.esciencecenter.word2vec.data.Vocabulary;
-import nl.esciencecenter.word2vec.data.Word;
 
 import java.io.BufferedReader;
 import java.io.IOException;
