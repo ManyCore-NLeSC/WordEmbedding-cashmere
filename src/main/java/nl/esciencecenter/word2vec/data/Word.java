c7c13b57d4ab56e1ad0d78f76e7757971640e384
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-10-19 11:49:20 +0200

    Added binary code and pointers to words.

--- src/main/java/nl/esciencecenter/word2vec/data/Word.java
+++ src/main/java/nl/esciencecenter/word2vec/data/Word.java
@@ -1,8 +1,12 @@
 package nl.esciencecenter.word2vec.data;
 
+import java.util.ArrayList;
+
 public class Word {
     private Integer occurrences;
     private String word;
+    private ArrayList<Character> code;
+    private ArrayList<Integer> points;
 
     public Word(String word) {
         occurrences = 0;
@@ -24,5 +28,25 @@ public class Word {
 
     public String getWord() {
         return word;
+    }
+
+    public Integer getCodeLength() {
+        return code.size();
+    }
+
+    public void setCode(ArrayList<Character> code) {
+        this.code = code;
+    }
+
+    public ArrayList<Character> getCode() {
+        return code;
+    }
+
+    public void setPoints(ArrayList<Integer> points) {
+        this.points = points;
+    }
+
+    public ArrayList<Integer> getPoints() {
+        return points;
     }
 }
