3c986ae001ac8fe0d7cd6616b832f827bf3b336f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-10-31 15:16:22 +0100

    First complete draft of the main application.

--- src/main/java/nl/esciencecenter/word2vec/Word2Vec.java
+++ src/main/java/nl/esciencecenter/word2vec/Word2Vec.java
@@ -2,6 +2,7 @@ package nl.esciencecenter.word2vec;
 
 import com.beust.jcommander.JCommander;
 import nl.esciencecenter.word2vec.data.Vocabulary;
+import nl.esciencecenter.word2vec.network.NeuralNetwork;
 import nl.esciencecenter.word2vec.utilities.LearnVocabulary;
 import nl.esciencecenter.word2vec.utilities.ReadVocabulary;
 import nl.esciencecenter.word2vec.utilities.SaveVocabulary;
@@ -21,11 +22,9 @@ public class Word2Vec {
             commander.usage();
             return;
         }
-
+        // Read vocabulary
         vocabulary = new Vocabulary(arguments.getMinCount());
         vocabulary.setMaxSize(arguments.getVocabularyMaxSize());
-
-        // Read vocabulary
         if ( arguments.getInVocabularyFilename().length() > 0 ) {
             BufferedReader inVocabularyFile;
 
@@ -37,7 +36,8 @@ public class Word2Vec {
                 err.printStackTrace();
             }
             if ( arguments.getDebug() ) {
-                System.err.println("Read vocabulary from file \"" + arguments.getInVocabularyFilename() + "\"; the vocabulary contains " + vocabulary.getNrWords() + " words.");
+                System.err.println("Read vocabulary from file \"" + arguments.getInVocabularyFilename()
+                        + "\"; the vocabulary contains " + vocabulary.getNrWords() + " words.");
             }
         } else {
             BufferedReader trainingFile;
@@ -50,12 +50,31 @@ public class Word2Vec {
                 err.printStackTrace();
             }
             if ( arguments.getDebug() ) {
-                System.err.println("Learned vocabulary from file \"" + arguments.getTrainingFilename() + "\"; the vocabulary contains " + vocabulary.getNrWords() + " words.");
+                System.err.println("Learned vocabulary from file \"" + arguments.getTrainingFilename()
+                        + "\"; the vocabulary contains " + vocabulary.getNrWords() + " words.");
             }
         }
         // Sort vocabulary
         vocabulary.sort();
+        // Initialize neural network
+        NeuralNetwork neuralNetwork = new NeuralNetwork(arguments.getUseCBOW(), arguments.getSoftmax(),
+                arguments.getUsePosition(), arguments.getNegativeSamples(), arguments.getVectorDimensions(),
+                arguments.getWindowSize(), arguments.getAlpha());
+        neuralNetwork.initializeExponentialTable();
+        neuralNetwork.initialize(vocabulary);
+        if ( arguments.getNegativeSamples() > 0 ) {
+            neuralNetwork.initializeUnigramTable(vocabulary);
+        }
+        // Train network
+        try {
+            BufferedReader trainingFile;
 
+            trainingFile = new BufferedReader(new FileReader(arguments.getTrainingFilename()));
+            neuralNetwork.trainModel(vocabulary, trainingFile);
+            trainingFile.close();
+        } catch ( IOException err ) {
+            err.printStackTrace();
+        }
         // Save vocabulary
         if ( arguments.getOutVocabularyFilename().length() > 0 ) {
             BufferedWriter outVocabularyFile;
@@ -67,6 +86,24 @@ public class Word2Vec {
             } catch ( IOException err ) {
                 err.printStackTrace();
             }
+        }
+        // Save learned vectors
+        BufferedWriter outputFile;
+        try {
+            outputFile = new BufferedWriter(new FileWriter(arguments.getOutputFilename()));
+            if ( arguments.getClasses() > 0 ) {
+                neuralNetwork.saveWordVectors(vocabulary, outputFile);
+            } else {
+                neuralNetwork.saveClasses(vocabulary, outputFile, arguments.getClasses());
+            }
+            outputFile.close();
+            if ( !arguments.getOutContextVectorsFilename().isEmpty() ) {
+                outputFile = new BufferedWriter(new FileWriter(arguments.getOutContextVectorsFilename()));
+                neuralNetwork.saveContextVectors(vocabulary, outputFile);
+                outputFile.close();
+            }
+        } catch ( IOException err ) {
+            err.printStackTrace();
         }
     }
 }
