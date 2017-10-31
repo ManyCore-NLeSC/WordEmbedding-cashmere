14aa4d3b76b52ce8385bc32f5319e0b05c3a175f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2017-10-31 15:03:51 +0100

    Add output filename to command line arguments.

--- src/main/java/nl/esciencecenter/word2vec/CommandLineArguments.java
+++ src/main/java/nl/esciencecenter/word2vec/CommandLineArguments.java
@@ -5,13 +5,19 @@ import com.beust.jcommander.Parameter;
 public class CommandLineArguments {
     @Parameter(names = {"-help", "-h"}, help = true)
     private Boolean help = false;
-    @Parameter(names = {"-training_file", "-train"}, description = "Text file containing the data to train the model.", required = true)
+    @Parameter(names = {"-training_file", "-train"}, description = "Text file containing the data to train the model.",
+            required = true)
     private String trainingFilename = "";
-    @Parameter(names = {"-occurrence_threshold", "-min-count"}, description = "Only consider words that occur more than the threshold; default is 5.")
+    @Parameter(names = {"-output_file", "-output"}, description = "Output file.")
+    private String outputFilename = "";
+    @Parameter(names = {"-occurrence_threshold", "-min-count"},
+            description = "Only consider words that occur more than the threshold; default is 5.")
     private Integer minCount = 5;
-    @Parameter(names = {"-store_vocabulary", "-save-vocab"}, description = "File where to store the learned vocabulary.")
+    @Parameter(names = {"-store_vocabulary", "-save-vocab"},
+            description = "File where to store the learned vocabulary.")
     private String outVocabularyFilename = "";
-    @Parameter(names = {"-load_vocabulary", "-read-vocab"}, description = "File to load a previously learned vocabulary from.")
+    @Parameter(names = {"-load_vocabulary", "-read-vocab"},
+            description = "File to load a previously learned vocabulary from.")
     private String inVocabularyFilename = "";
     @Parameter(names = {"-vocabulary_max_size"}, description = "Maximum number of words in the vocabulary.")
     private Integer vocabularyMaxSize = 30000000;
@@ -48,6 +54,10 @@ public class CommandLineArguments {
 
     public String getTrainingFilename() {
         return trainingFilename;
+    }
+
+    public String getOutputFilename() {
+        return outputFilename;
     }
 
     public Integer getMinCount() {
