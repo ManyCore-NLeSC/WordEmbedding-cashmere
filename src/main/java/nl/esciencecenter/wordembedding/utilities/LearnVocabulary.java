3403e34bfb22d311985a832513f4bdd90f60905b
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-01-22 14:42:10 +0100

    LearnVocabulary is multi-threaded. I am not saying that this is a good idea, but I felt like experimenting with it.

--- src/main/java/nl/esciencecenter/wordembedding/utilities/LearnVocabulary.java
+++ src/main/java/nl/esciencecenter/wordembedding/utilities/LearnVocabulary.java
@@ -5,29 +5,43 @@ import nl.esciencecenter.wordembedding.data.Vocabulary;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class LearnVocabulary {
-    public static void learn(Vocabulary vocabulary, BufferedReader fileReader, Boolean strict) throws IOException {
-        final Double fillingThreshold = 0.7;
+public class LearnVocabulary extends Thread {
+    private static double fillingThreshold = 0.7;
+    private final Vocabulary vocabulary;
+    private final BufferedReader fileReader;
+    private final Boolean strict;
+
+
+    public LearnVocabulary(Vocabulary vocabulary, BufferedReader fileReader, Boolean strict) {
+        this.vocabulary = vocabulary;
+        this.fileReader = fileReader;
+        this.strict = strict;
+    }
+
+    public void run() {
         String line;
 
-        while ( (line = fileReader.readLine()) != null ) {
-            vocabulary.addWord("</s>");
-            while ( !line.isEmpty() ) {
-                String word = ReadWord.readWord(line, strict);
+        try {
+            while ( (line = fileReader.readLine()) != null ) {
+                vocabulary.addWord("</s>");
+                while ( !line.isEmpty() ) {
+                    String word = ReadWord.readWord(line, strict);
 
-                if ( word == null ) {
-                    line = line.trim();
-                    continue;
-                } else {
-                    line = line.substring(word.length());
-                    line = line.trim();
-                }
-                vocabulary.addWord(word);
-                if ( vocabulary.getNrWords() > vocabulary.getMaxSize() * fillingThreshold ) {
-                    vocabulary.reduce();
+                    if ( word == null ) {
+                        line = line.trim();
+                        continue;
+                    } else {
+                        line = line.substring(word.length());
+                        line = line.trim();
+                    }
+                    vocabulary.addWord(word);
+                    if ( vocabulary.getNrWords() > vocabulary.getMaxSize() * fillingThreshold ) {
+                        vocabulary.reduce();
+                    }
                 }
             }
+        } catch ( IOException err ) {
+            return;
         }
-        vocabulary.reduce();
     }
 }
