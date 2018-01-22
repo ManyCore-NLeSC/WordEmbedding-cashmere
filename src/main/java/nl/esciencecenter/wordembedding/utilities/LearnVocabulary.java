f4f194c65513096246d0234407529664531be6c8
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-01-22 15:55:51 +0100

    Print the stack trace.

--- src/main/java/nl/esciencecenter/wordembedding/utilities/LearnVocabulary.java
+++ src/main/java/nl/esciencecenter/wordembedding/utilities/LearnVocabulary.java
@@ -41,7 +41,7 @@ public class LearnVocabulary extends Thread {
                 }
             }
         } catch ( IOException err ) {
-            return;
+            err.printStackTrace();
         }
     }
 }
