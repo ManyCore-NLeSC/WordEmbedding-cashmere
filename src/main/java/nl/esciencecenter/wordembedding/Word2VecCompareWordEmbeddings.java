cd30a7806aa19ae15a5be34a616220ad2020b86b
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-07 15:31:55 +0100

    Add an utility to compare word embeddings.

---
+++ src/main/java/nl/esciencecenter/wordembedding/Word2VecCompareWordEmbeddings.java
@@ -0,0 +1,38 @@
+package nl.esciencecenter.wordembedding;
+
+import nl.esciencecenter.wordembedding.data.WordEmbedding;
+import nl.esciencecenter.wordembedding.utilities.ReadWord2VecWordVector;
+import nl.esciencecenter.wordembedding.validation.CompareWordEmbeddings;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Word2VecCompareWordEmbeddings {
+    public static void main(String [] args) {
+        BufferedReader embeddingFile;
+        WordEmbedding [] embeddings;
+        if ( args.length < 2 ) {
+            System.err.println("Usage: Word2VecCompareWordEmbeddings <embedding_file> ... <embedding_file>");
+            return;
+        }
+        // Load vocabularies from file
+        embeddings = new WordEmbedding [args.length];
+        for ( int argument = 0; argument < args.length; argument++ ) {
+            try {
+                embeddingFile = new BufferedReader(new FileReader(args[argument]));
+                embeddings[argument] = ReadWord2VecWordVector.read(embeddingFile);
+                embeddingFile.close();
+            } catch ( IOException err ) {
+                System.err.println("Impossible to open \"" + args[argument] + "\".");
+                return;
+            }
+        }
+        // Compare
+        if ( CompareWordEmbeddings.compare(embeddings) ) {
+            System.out.println("The embeddings are equal.");
+        } else  {
+            System.out.println("The embeddings are different.");
+        }
+    }
+}
