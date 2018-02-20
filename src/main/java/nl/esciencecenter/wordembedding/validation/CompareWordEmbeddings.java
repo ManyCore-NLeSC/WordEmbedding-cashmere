b4becaaadca1e3f105531cf4fc07e3f6ff635b5b
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-20 15:44:24 +0100

    Compare similarity of the nearest neighbors.

--- src/main/java/nl/esciencecenter/wordembedding/validation/CompareWordEmbeddings.java
+++ src/main/java/nl/esciencecenter/wordembedding/validation/CompareWordEmbeddings.java
@@ -2,13 +2,28 @@ package nl.esciencecenter.wordembedding.validation;
 
 import nl.esciencecenter.wordembedding.data.WordEmbedding;
 import nl.esciencecenter.wordembedding.math.Cosine;
+import nl.esciencecenter.wordembedding.math.FloatComparison;
+import nl.esciencecenter.wordembedding.utilities.NearestNeighborsWordEmbedding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class CompareWordEmbeddings {
-    public static Boolean compareIdentity(WordEmbedding [] embeddings) {
-        return compareIdentity(embeddings, false);
+    public static Boolean compareDimensionality(WordEmbedding [] embeddings) {
+        for ( int embeddingID = 1; embeddingID < embeddings.length; embeddingID++ ) {
+            if ( !embeddings[embeddingID].getVectorDimensions().equals(embeddings[0].getVectorDimensions()) ) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    public static Boolean compareIdentity(WordEmbedding [] embeddings, Boolean allowError) {
+    public static Boolean compareNumericalIdentity(WordEmbedding [] embeddings) {
+        return compareNumericalIdentity(embeddings, false);
+    }
+
+    public static Boolean compareNumericalIdentity(WordEmbedding [] embeddings, Boolean allowError) {
         if ( embeddings.length > 1 ) {
             for ( int embeddingID = 1; embeddingID < embeddings.length; embeddingID++ ) {
                 for ( String referenceWord : embeddings[0].getWords() ) {
@@ -19,11 +34,13 @@ public class CompareWordEmbeddings {
                     }
                     for ( int dimension = 0; dimension < embeddings[0].getVectorDimensions(); dimension++ ) {
                         if ( allowError ) {
-                            if ( !same(referenceCoordinates[dimension], coordinates[dimension], 1.0e-06f) ) {
+                            if ( !FloatComparison.areSimilar(referenceCoordinates[dimension],
+                                    coordinates[dimension], 1.0e-06f) ) {
                                 return false;
                             }
                         } else {
-                            if ( !identical(referenceCoordinates[dimension], coordinates[dimension]) ) {
+                            if ( !FloatComparison.areIdentical(referenceCoordinates[dimension],
+                                    coordinates[dimension]) ) {
                                 return false;
                             }
                         }
@@ -34,7 +51,7 @@ public class CompareWordEmbeddings {
         return true;
     }
 
-    public static Boolean compareSimilarity(WordEmbedding []  embeddings) {
+    public static Boolean compareNumericalSimilarity(WordEmbedding []  embeddings) {
         if ( embeddings.length > 1 ) {
             for ( String wordOne : embeddings[0].getWords() ) {
                 for ( String wordTwo : embeddings[0].getWords() ) {
@@ -44,7 +61,7 @@ public class CompareWordEmbeddings {
                     for ( int embedding = 1; embedding < embeddings.length; embedding++ ) {
                         Float testedCosine = Cosine.cosine(embeddings[embedding].getWordCoordinates(wordOne),
                                 embeddings[embedding].getWordCoordinates(wordTwo));
-                        if ( !same(referenceCosine, testedCosine, 0.1f) ) {
+                        if ( !FloatComparison.areSimilar(referenceCosine, testedCosine, 0.1f) ) {
                             return false;
                         }
                     }
@@ -54,20 +71,30 @@ public class CompareWordEmbeddings {
         return true;
     }
 
-    public static Boolean testDimensionality(WordEmbedding [] embeddings) {
-        for ( int embeddingID = 1; embeddingID < embeddings.length; embeddingID++ ) {
-            if ( !embeddings[embeddingID].getVectorDimensions().equals(embeddings[0].getVectorDimensions()) ) {
-                return false;
+    public static Float compareNearestNeighbors(WordEmbedding [] embeddings, Integer percentage) {
+        Float averageNeighborhoodIntersection = 0.0f;
+        HashMap<String, HashMap<Integer, ArrayList<String>>> neighbors
+                = NearestNeighborsWordEmbedding.compute(embeddings);
+
+        for ( String referenceWord : embeddings[0].getWords() ) {
+            HashSet<String> neighborhoodIntersection = new HashSet<>();
+
+            for ( int embedding = 0; embedding < embeddings.length; embedding++ ) {
+                HashSet<String> neighborhood = new HashSet<>();
+
+                for ( int neighborID = 0;
+                      neighborID < ((embeddings[0].getNrWords() - 1) * percentage) / 100;
+                      neighborID++ ) {
+                    neighborhood.add(neighbors.get(referenceWord).get(embedding).get(neighborID));
+                }
+                if ( embedding == 0 ) {
+                    neighborhoodIntersection.addAll(neighborhood);
+                } else {
+                    neighborhoodIntersection.retainAll(neighborhood);
+                }
             }
+            averageNeighborhoodIntersection += neighborhoodIntersection.size();
         }
-        return true;
-    }
-
-    private static Boolean identical(Float x, Float y) {
-        return x.compareTo(y) == 0;
-    }
-
-    private static Boolean same(Float x, Float y, Float error) {
-        return Math.abs(x - y) < error;
+        return averageNeighborhoodIntersection / embeddings[0].getNrWords();
     }
 }
