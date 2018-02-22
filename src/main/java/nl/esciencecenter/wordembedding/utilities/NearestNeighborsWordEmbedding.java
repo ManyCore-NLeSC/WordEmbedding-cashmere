29355f677b92db748fdeb4e5ebbc127a61a1c895
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-22 11:04:18 +0100

    Use NearestNeighborWord.

--- src/main/java/nl/esciencecenter/wordembedding/utilities/NearestNeighborsWordEmbedding.java
+++ src/main/java/nl/esciencecenter/wordembedding/utilities/NearestNeighborsWordEmbedding.java
@@ -1,33 +1,22 @@
 package nl.esciencecenter.wordembedding.utilities;
 
 import nl.esciencecenter.wordembedding.data.WordEmbedding;
-import nl.esciencecenter.wordembedding.math.Cosine;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 
 public class NearestNeighborsWordEmbedding {
-    public static HashMap<String, HashMap<Integer, ArrayList<String>>> compute(WordEmbedding[] embeddings) {
-        HashMap<String, HashMap<Integer, ArrayList<String>>> neighbors = new HashMap<>();
+    public static HashMap<String, HashMap<Integer, String []>> compute(WordEmbedding[] embeddings) {
+        HashMap<String, HashMap<Integer, String []>> neighbors = new HashMap<>();
 
         for ( String referenceWord : embeddings[0].getWords() ) {
             neighbors.put(referenceWord, new HashMap<>());
             for ( int embedding = 0; embedding < embeddings.length; embedding++ ) {
-                final int finalEmbedding = embedding;
-
                 if ( !embeddings[embedding].getWords().contains(referenceWord) ) {
                     neighbors.remove(referenceWord);
                     break;
                 }
-                neighbors.get(referenceWord).put(embedding, new ArrayList<>(embeddings[embedding].getWords()));
-                neighbors.get(referenceWord).get(embedding).remove(referenceWord);
-                neighbors.get(referenceWord).get(embedding).sort((stringOne, stringTwo)
-                        -> Float.compare(Cosine.cosine(embeddings[finalEmbedding].getWordCoordinates(referenceWord),
-                        embeddings[finalEmbedding].getWordCoordinates(stringOne)),
-                        Cosine.cosine(embeddings[finalEmbedding].getWordCoordinates(referenceWord),
-                                embeddings[finalEmbedding].getWordCoordinates(stringTwo))));
-                Collections.reverse(neighbors.get(referenceWord).get(embedding));
+                neighbors.get(referenceWord).put(embedding,
+                        NearestNeighborsWord.compute(referenceWord, embeddings[embedding]));
             }
         }
         return neighbors;
