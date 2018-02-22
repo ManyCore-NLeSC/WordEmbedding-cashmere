22946c3533d39f324edb14424e5a5a820429200c
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-02-22 15:40:22 +0100

    Vector sum.

---
+++ src/main/java/nl/esciencecenter/wordembedding/math/Sum.java
@@ -0,0 +1,12 @@
+package nl.esciencecenter.wordembedding.math;
+
+public class Sum {
+    public static Float [] compute(Float [] vectorOne, Float [] vectorTwo) {
+        Float [] resultingVector = new Float [vectorOne.length];
+
+        for ( int dimension = 0; dimension < vectorOne.length; dimension++ ) {
+            resultingVector[dimension] = vectorOne[dimension] + vectorTwo[dimension];
+        }
+        return resultingVector;
+    }
+}
