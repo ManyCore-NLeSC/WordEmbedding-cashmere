666da3e5f81fc7011323f22578f602274e01a8b4
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-03-01 14:10:58 +0100

    Stub. It needs to compute the ranks before calling the Pearson correlation.

---
+++ src/main/java/nl/esciencecenter/wordembedding/math/SpearmanRankCorrelation.java
@@ -0,0 +1,9 @@
+package nl.esciencecenter.wordembedding.math;
+
+import java.util.ArrayList;
+
+public class SpearmanRankCorrelation {
+    public static Float compute(ArrayList<Float []> vectors) {
+        return PearsonCorrelationCoefficient.compute(vectors);
+    }
+}
