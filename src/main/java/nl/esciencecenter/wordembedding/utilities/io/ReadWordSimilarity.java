2ee93fba8e1dbb49927e189826712f326d64102f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-03-01 11:58:03 +0100

    Reads a test file containing ground truth for word similarity.

---
+++ src/main/java/nl/esciencecenter/wordembedding/utilities/io/ReadWordSimilarity.java
@@ -0,0 +1,17 @@
+package nl.esciencecenter.wordembedding.utilities.io;
+
+import nl.esciencecenter.wordembedding.data.WordSimilarity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ReadWordSimilarity {
+    public static void read(WordSimilarity similarities, BufferedReader fileReader) throws IOException {
+        String line;
+
+        while ( (line = fileReader.readLine()) != null ) {
+            String [] values = line.split("[ \t]");
+            similarities.addSimilarityScore(values[0], values[1], Float.parseFloat(values[2]));
+        }
+    }
+}
