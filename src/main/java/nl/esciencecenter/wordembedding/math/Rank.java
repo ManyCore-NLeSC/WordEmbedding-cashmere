9e5a9aab19dbf8c56820d396842438bffc243f82
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-03-01 14:13:01 +0100

    Stub for Rank.

---
+++ src/main/java/nl/esciencecenter/wordembedding/math/Rank.java
@@ -0,0 +1,9 @@
+package nl.esciencecenter.wordembedding.math;
+
+import java.util.ArrayList;
+
+public class Rank {
+    public static ArrayList<Float []> compute(ArrayList<Float []> vectors) {
+        return vectors;
+    }
+}
