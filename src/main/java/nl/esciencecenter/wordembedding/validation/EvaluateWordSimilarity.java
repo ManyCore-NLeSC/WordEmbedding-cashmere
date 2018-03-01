ee5580ff88c273096ea9a3af044d65493ac4ae6b
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-03-01 12:00:22 +0100

    Evaluate the word similarity between an embedding and a given ground truth.

---
+++ src/main/java/nl/esciencecenter/wordembedding/validation/EvaluateWordSimilarity.java
@@ -0,0 +1,30 @@
+package nl.esciencecenter.wordembedding.validation;
+
+import nl.esciencecenter.wordembedding.data.WordEmbedding;
+import nl.esciencecenter.wordembedding.data.WordSimilarity;
+import nl.esciencecenter.wordembedding.math.Cosine;
+import nl.esciencecenter.wordembedding.math.PearsonCorrelationCoefficient;
+
+import java.util.ArrayList;
+
+public class EvaluateWordSimilarity {
+    public static Float correlation(WordSimilarity similarity, WordEmbedding embedding) {
+        ArrayList<Float []> results = new ArrayList<>();
+
+        for ( String wordOne : similarity.getWords() ) {
+            for ( String wordTwo : similarity.getWords(wordOne) ) {
+                Float [] pair = new Float [2];
+
+                if ( (embedding.getWordCoordinates(wordOne) != null)
+                        && (embedding.getWordCoordinates(wordTwo) != null) ) {
+                    pair[0] = similarity.getSimilarityScore(wordOne, wordTwo);
+                    pair[1] = Cosine.compute(embedding.getWordCoordinates(wordOne),
+                            embedding.getWordCoordinates(wordTwo));
+                    results.add(pair);
+                }
+            }
+        }
+
+        return PearsonCorrelationCoefficient.compute(results);
+    }
+}
