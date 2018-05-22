d274ef015eb93eb81fc8b091c7a74dbae9df32f3
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-05-22 14:38:37 +0200

    Read binary file if the extension is not ".txt".

--- src/main/java/nl/esciencecenter/wordembedding/utilities/io/ReadWord2VecVectors.java
+++ src/main/java/nl/esciencecenter/wordembedding/utilities/io/ReadWord2VecVectors.java
@@ -3,6 +3,9 @@ package nl.esciencecenter.wordembedding.utilities.io;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
 
 public class ReadWord2VecVectors {
     public static float [] read(String fileName) throws IOException
@@ -13,21 +16,31 @@ public class ReadWord2VecVectors {
         String [] values;
         float [] vectors;
 
-        BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
-        // Read the first line
-        line = fileReader.readLine();
-        values = line.split(" ");
-        nrWords = Integer.parseInt(values[0]);
-        dimensions = Integer.parseInt(values[1]);
-        vectors = new float [nrWords * dimensions];
-        // Read the vectors
-        int wordIndex = 0;
-        while ( (line = fileReader.readLine()) != null ) {
+        if (fileName.contains(".txt"))
+        {
+            BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
+            // Read the first line
+            line = fileReader.readLine();
             values = line.split(" ");
-            for ( int dimension = 0; dimension < dimensions; dimension++ ) {
-                vectors[(wordIndex * dimensions) + dimension] = Float.parseFloat(values[dimension + 1]);
+            nrWords = Integer.parseInt(values[0]);
+            dimensions = Integer.parseInt(values[1]);
+            vectors = new float [nrWords * dimensions];
+            // Read the vectors
+            int wordIndex = 0;
+            while ( (line = fileReader.readLine()) != null ) {
+                values = line.split(" ");
+                for ( int dimension = 0; dimension < dimensions; dimension++ ) {
+                    vectors[(wordIndex * dimensions) + dimension] = Float.parseFloat(values[dimension + 1]);
+                }
+                wordIndex++;
             }
-            wordIndex++;
+            fileReader.close();
+        }
+        else
+        {
+            FileChannel fileReader = new RandomAccessFile(fileName, "r").getChannel();
+            vectors = fileReader.map(FileChannel.MapMode.READ_ONLY, 0, fileReader.size()).order(ByteOrder.nativeOrder()).asFloatBuffer().array();
+            fileReader.close();
         }
         return vectors;
     }
