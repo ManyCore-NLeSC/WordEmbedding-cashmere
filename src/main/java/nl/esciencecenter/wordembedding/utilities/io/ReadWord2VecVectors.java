0b227a2eae54f451ec023390efd1f96f115a9b90
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-05-22 16:24:33 +0200

    Read a C generated binary.

--- src/main/java/nl/esciencecenter/wordembedding/utilities/io/ReadWord2VecVectors.java
+++ src/main/java/nl/esciencecenter/wordembedding/utilities/io/ReadWord2VecVectors.java
@@ -5,19 +5,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 import java.nio.channels.FileChannel;
 
 public class ReadWord2VecVectors {
     public static float [] read(String fileName) throws IOException
     {
-        int nrWords;
-        int dimensions;
-        String line;
-        String [] values;
         float [] vectors;
 
         if (fileName.contains(".txt"))
         {
+            int nrWords;
+            int dimensions;
+            String line;
+            String [] values;
             BufferedReader fileReader = new BufferedReader(new FileReader(fileName));
             // Read the first line
             line = fileReader.readLine();
@@ -39,7 +40,13 @@ public class ReadWord2VecVectors {
         else
         {
             FileChannel fileReader = new RandomAccessFile(fileName, "r").getChannel();
-            vectors = fileReader.map(FileChannel.MapMode.READ_ONLY, 0, fileReader.size()).order(ByteOrder.nativeOrder()).asFloatBuffer().array();
+            int nrElements = (int)(fileReader.size() / 4);
+            vectors = new float [nrElements];
+            FloatBuffer buffer = fileReader.map(FileChannel.MapMode.READ_ONLY, 0, fileReader.size()).order(ByteOrder.nativeOrder()).asFloatBuffer();
+            for (int element = 0; element < nrElements; element++)
+            {
+                vectors[element] = buffer.get(element);
+            }
             fileReader.close();
         }
         return vectors;
