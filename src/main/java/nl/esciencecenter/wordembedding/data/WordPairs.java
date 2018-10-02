071650f9c93a42c7da19e5b08769e1cfbdb96099
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-10-02 16:35:54 +0200

    First implementation of WordPairs to store all the pair occurrences.

---
+++ src/main/java/nl/esciencecenter/wordembedding/data/WordPairs.java
@@ -0,0 +1,47 @@
+package nl.esciencecenter.wordembedding.data;
+
+import java.util.LinkedHashMap;
+
+public class WordPairs {
+    private final String separator = "_<%%>_";
+    private int occurrences;
+    private LinkedHashMap<String, Integer> coOccurrences;
+
+    public WordPairs()
+    {
+        this.coOccurrences = new LinkedHashMap<>();
+        this.occurrences = 0;
+    }
+
+    public int getTotalPairs()
+    {
+        return this.occurrences;
+    }
+
+    public void addPair(String wordOne, String wordTwo)
+    {
+        if ( coOccurrences.containsKey(wordOne + separator + wordTwo) )
+        {
+            coOccurrences.put(wordOne + separator + wordTwo, coOccurrences.get(wordOne + separator + wordTwo) + 1);
+        }
+        else if ( coOccurrences.containsKey(wordTwo + separator + wordOne) )
+        {
+            coOccurrences.put(wordTwo + separator + wordOne, coOccurrences.get(wordTwo + separator + wordOne) + 1);
+        }
+        else
+        {
+            coOccurrences.put(wordOne + separator + wordTwo, 1);
+        }
+        occurrences++;
+    }
+
+    public int getPairOccurrences(String wordOne, String wordTwo)
+    {
+        Integer pairOccurrences = coOccurrences.get(wordOne + separator + wordTwo);
+        if ( pairOccurrences == null )
+        {
+            pairOccurrences = coOccurrences.get(wordTwo + separator + wordOne);
+        }
+        return pairOccurrences;
+    }
+}
