40faec48edb6a21cfb4d8c55426d2694cd4da7ce
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-10-30 14:38:56 +0100

    Command line arguments for PMI comparison.

---
+++ src/main/java/nl/esciencecenter/wordembedding/commandline/Word2VecPMIComparisonCommandLineArguments.java
@@ -0,0 +1,63 @@
+package nl.esciencecenter.wordembedding.commandline;
+
+import com.beust.jcommander.Parameter;
+
+public class Word2VecPMIComparisonCommandLineArguments
+{
+    @Parameter(names = {"-help", "-h"}, help = true)
+    private boolean help = false;
+    @Parameter(names = {"-vocabulary_file"}, description = "Word2Vec vocabulary file.", required = true)
+    private String vocabularyFilename = "";
+    @Parameter(names = {"-window"}, description = "Window size for pair generation.")
+    private int window = 2;
+    @Parameter(names = {"-corpus_file"}, description = "Corpus file.", required = true)
+    private String corpusFilename = "";
+    @Parameter(names = {"-vector_file"}, description = "Word2Vec vector file.", required = true)
+    private String vectorFilename = "";
+    @Parameter(names = {"-context_file"}, description = "Word2Vec context file.", required = true)
+    private String contextFilename = "";
+    @Parameter(names = {"-max"}, description = "Compute the maximum difference.")
+    private boolean max = false;
+    @Parameter(names = {"-min"}, description = "Compute the minimum difference.")
+    private boolean min = false;
+
+    public boolean getHelp()
+    {
+        return help;
+    }
+
+    public String getVocabularyFileName()
+    {
+        return vocabularyFilename;
+    }
+
+    public int getWindow()
+    {
+        return window;
+    }
+
+    public String getCorpusFileName()
+    {
+        return corpusFilename;
+    }
+
+    public String getVectorFileName()
+    {
+        return vectorFilename;
+    }
+
+    public String getContextFileName()
+    {
+        return contextFilename;
+    }
+
+    public boolean getMax()
+    {
+        return max;
+    }
+
+    public boolean getMin()
+    {
+        return min;
+    }
+}
