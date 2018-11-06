059088b9911b27c156a58bf1e18a1abd19e4095f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-11-06 11:48:30 +0100

    Fixed mean computation.

--- src/main/java/nl/esciencecenter/wordembedding/math/Mean.java
+++ src/main/java/nl/esciencecenter/wordembedding/math/Mean.java
@@ -4,12 +4,13 @@ public class Mean
 {
     public static float compute(float [][] vector)
     {
-        long nrElements = 1;
+        long nrElements = 0;
         float mean = 0;
         for ( int itemOne = 0; itemOne < vector.length; itemOne++ )
         {
             for ( int itemTwo = 0; itemTwo < vector[itemOne].length; itemTwo++ )
             {
+                nrElements++;
                 if ( nrElements == 1 )
                 {
                     mean = vector[itemOne][itemTwo];
