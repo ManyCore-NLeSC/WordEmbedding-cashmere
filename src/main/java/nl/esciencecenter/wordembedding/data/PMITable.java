9f888a4f7f9cb80ed1973d479c27915bd71d239f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-11-06 18:36:07 +0100

    Remove positive PMI.

--- src/main/java/nl/esciencecenter/wordembedding/data/PMITable.java
+++ src/main/java/nl/esciencecenter/wordembedding/data/PMITable.java
@@ -13,7 +13,7 @@ public class PMITable
 
     public float getPMI(String wordOne, String wordTwo)
     {
-        return Math.max(log2(((float)(pairs.getPairOccurrences(wordOne, wordTwo)) / pairs.getTotalPairs()) / (((float)(vocabulary.getWord(wordOne).getOccurrences()) / (vocabulary.getOccurrences() - vocabulary.getWord("</s>").getOccurrences())) * (((float)(vocabulary.getWord(wordTwo).getOccurrences()) / (vocabulary.getOccurrences() - vocabulary.getWord("</s>").getOccurrences()))))), 0.0f);
+        return log2(((float)(pairs.getPairOccurrences(wordOne, wordTwo)) / pairs.getTotalPairs()) / (((float)(vocabulary.getWord(wordOne).getOccurrences()) / (vocabulary.getOccurrences() - vocabulary.getWord("</s>").getOccurrences())) * (((float)(vocabulary.getWord(wordTwo).getOccurrences()) / (vocabulary.getOccurrences() - vocabulary.getWord("</s>").getOccurrences())))));
     }
 
     private float log2(float x)
