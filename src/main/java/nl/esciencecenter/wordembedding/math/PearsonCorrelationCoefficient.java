0b69d1989c1910d67b717602496b927703321a3c
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-11-13 14:27:49 +0100

    Just style.

--- src/main/java/nl/esciencecenter/wordembedding/math/PearsonCorrelationCoefficient.java
+++ src/main/java/nl/esciencecenter/wordembedding/math/PearsonCorrelationCoefficient.java
@@ -2,22 +2,24 @@ package nl.esciencecenter.wordembedding.math;
 
 import java.util.ArrayList;
 
-public class PearsonCorrelationCoefficient {
-    public static float compute(ArrayList<float []> vectors) {
+public class PearsonCorrelationCoefficient
+{
+    public static float compute(ArrayList<float []> vectors)
+    {
         float sumXY = 0.0f;
         float sumX = 0.0f;
         float sumX2 = 0.0f;
         float sumY = 0.0f;
         float sumY2 = 0.0f;
 
-        for (float [] vector : vectors) {
+        for (float [] vector : vectors)
+        {
             sumXY += vector[0] * vector[1];
             sumX += vector[0];
             sumX2 += vector[0] * vector[0];
             sumY += vector[1];
             sumY2 += vector[1] * vector[1];
         }
-        return (float)(((vectors.size() * sumXY) - (sumX * sumY))
-                / Math.sqrt(((vectors.size() * sumX2) - (sumX * sumX)) * ((vectors.size() * sumY2) - (sumY * sumY))));
+        return (float)(((vectors.size() * sumXY) - (sumX * sumY)) / Math.sqrt(((vectors.size() * sumX2) - (sumX * sumX)) * ((vectors.size() * sumY2) - (sumY * sumY))));
     }
 }
--- src/main/java/nl/esciencecenter/wordembedding/math/SpearmanRankCorrelation.java
+++ src/main/java/nl/esciencecenter/wordembedding/math/SpearmanRankCorrelation.java
@@ -2,8 +2,10 @@ package nl.esciencecenter.wordembedding.math;
 
 import java.util.ArrayList;
 
-public class SpearmanRankCorrelation {
-    public static float compute(ArrayList<float []> vectors) {
+public class SpearmanRankCorrelation
+{
+    public static float compute(ArrayList<float []> vectors)
+    {
         return PearsonCorrelationCoefficient.compute(Rank.compute(vectors));
     }
 }
