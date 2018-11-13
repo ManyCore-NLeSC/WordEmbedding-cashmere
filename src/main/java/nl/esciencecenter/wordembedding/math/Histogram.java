4bcfb32a10dd24fd9e0c88c2235a859d77f2908f
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-11-13 11:34:21 +0100

    Improved histogram.

--- src/main/java/nl/esciencecenter/wordembedding/math/Histogram.java
+++ src/main/java/nl/esciencecenter/wordembedding/math/Histogram.java
@@ -10,18 +10,18 @@ public class Histogram
             for ( int itemTwo = itemOne; itemTwo < vector[itemOne].length; itemTwo++ ) {
                 if ( Float.isFinite(vector[itemOne][itemTwo]) )
                 {
-                    histogram[(int)((Math.abs(vector[itemOne][itemTwo]) / (max - min)) * (histogram.length - 1))]++;
+                    histogram[(int)(((vector[itemOne][itemTwo] - min) * (histogram.length - 1)) / (max - min))]++;
                 }
             }
         }
         return histogram;
     }
 
-    public static void print(long [] histogram)
+    public static void print(long [] histogram, float min, float max)
     {
         for ( int item = 0; item < histogram.length; item++ )
         {
-            System.out.println((item + 1) + ": " + histogram[item]);
+            System.out.println((item) + " " + (min + (item * ((max - min) / histogram.length))) + " " + histogram[item]);
         }
     }
 }
