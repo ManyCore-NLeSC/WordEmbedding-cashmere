024d2d43812f87d04378bb46746287397ec87a22
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-11-16 10:18:18 +0100

    Specify what you want to compute.

--- src/main/java/nl/esciencecenter/wordembedding/Word2VecPMIComparison.java
+++ src/main/java/nl/esciencecenter/wordembedding/Word2VecPMIComparison.java
@@ -26,16 +26,16 @@ public class Word2VecPMIComparison
         BufferedReader file;
         Vocabulary vocabulary;
         WordPairs pairs;
-        PMITable pmiTable;
-        WordEmbedding words, contexts;
+        PMITable pmiTable = null;
+        WordEmbedding words = null, contexts = null;
         Word2VecPMIComparisonCommandLineArguments arguments;
-        float [][] differences;
+        float [][] differences = null;
         // Statistics
         float [] min = new float[3];
         float [] max = new float[3];
         float [] mean = new float[3];
         float [] standardDeviation = new float[3];
-        long [] histogram;
+        long [][] histogram = new long[3][];
         float spearmanCorrelation = 0;
 
         // Command line arguments parsing
@@ -57,110 +57,162 @@ public class Word2VecPMIComparison
         vocabulary.sort();
         System.out.println("The vocabulary contains " + (vocabulary.getNrWords() - 1) +  " words; the total number of occurrences is " + (vocabulary.getOccurrences() - vocabulary.getWord("</s>").getOccurrences()) + ".");
         // Learn all pairs
-        try {
-            pairs = new WordPairs();
-            pairs.setWindowSize(arguments.getWindow());
-            file = new BufferedReader(new FileReader(arguments.getCorpusFileName()));
-            LearnWordPairs.learn(pairs, vocabulary, file);
-            file.close();
-        } catch ( IOException err ) {
-            System.err.println("Impossible to open \"" + arguments.getCorpusFileName() + "\".");
-            return;
-        }
-        System.out.println("The corpus contains " + pairs.getTotalPairs() + " word pairs.");
-        pmiTable = new PMITable(vocabulary, pairs);
-        // Read word and context vectors
-        try {
-            file = new BufferedReader(new FileReader(arguments.getVectorFileName()));
-            words = ReadWord2VecWordVectors.read(file);
-            file.close();
-        } catch ( IOException err ) {
-            System.err.println("Impossible to open \"" + arguments.getVectorFileName() + "\".");
-            return;
-        }
-        try {
-            file = new BufferedReader(new FileReader(arguments.getContextFileName()));
-            contexts = ReadWord2VecWordVectors.read(file);
-            file.close();
-        } catch ( IOException err ) {
-            System.err.println("Impossible to open \"" + arguments.getContextFileName() + "\".");
-            return;
-        }
-        differences = new float [vocabulary.getNrWords() - 1][vocabulary.getNrWords() - 1];
-        int wordOneIndex = 0;
-        for ( Word wordOne : vocabulary.getWords() )
+        if ( arguments.getPMI() || arguments.getPPMI() )
         {
-            if ( wordOne.getWord().equals("</s>") )
-            {
-                continue;
+            try {
+                pairs = new WordPairs();
+                pairs.setWindowSize(arguments.getWindow());
+                file = new BufferedReader(new FileReader(arguments.getCorpusFileName()));
+                LearnWordPairs.learn(pairs, vocabulary, file);
+                file.close();
+            } catch ( IOException err ) {
+                System.err.println("Impossible to open \"" + arguments.getCorpusFileName() + "\".");
+                return;
             }
-            int wordTwoIndex = 0;
-            for ( Word wordTwo : vocabulary.getWords() )
+            System.out.println("The corpus contains " + pairs.getTotalPairs() + " word pairs.");
+            pmiTable = new PMITable(vocabulary, pairs);
+        }
+        // Read word and context vectors
+        if ( arguments.getWord2Vec() )
+        {
+            try {
+                file = new BufferedReader(new FileReader(arguments.getVectorFileName()));
+                words = ReadWord2VecWordVectors.read(file);
+                file.close();
+            } catch ( IOException err ) {
+                System.err.println("Impossible to open \"" + arguments.getVectorFileName() + "\".");
+                return;
+            }
+            try {
+                file = new BufferedReader(new FileReader(arguments.getContextFileName()));
+                contexts = ReadWord2VecWordVectors.read(file);
+                file.close();
+            } catch ( IOException err ) {
+                System.err.println("Impossible to open \"" + arguments.getContextFileName() + "\".");
+                return;
+            }
+        }
+        if ( arguments.getWord2Vec() && (arguments.getPMI() || arguments.getPPMI()) && arguments.getDifference() )
+        {
+            differences = new float [vocabulary.getNrWords() - 1][vocabulary.getNrWords() - 1];
+            int wordOneIndex = 0;
+            for ( Word wordOne : vocabulary.getWords() )
             {
-                if ( wordTwo.getWord().equals("</s>") )
+                if ( wordOne.getWord().equals("</s>") )
                 {
                     continue;
                 }
-                if ( arguments.getPPMI() )
+                int wordTwoIndex = 0;
+                for ( Word wordTwo : vocabulary.getWords() )
                 {
-                    differences[wordOneIndex][wordTwoIndex] = DotProduct.compute(words.getWordCoordinates(wordOne.getWord()), contexts.getWordCoordinates(wordTwo.getWord())) - pmiTable.getPPMI(wordOne.getWord(), wordTwo.getWord());
+                    if ( wordTwo.getWord().equals("</s>") )
+                    {
+                        continue;
+                    }
+                    if ( arguments.getPPMI() )
+                    {
+                        differences[wordOneIndex][wordTwoIndex] = DotProduct.compute(words.getWordCoordinates(wordOne.getWord()), contexts.getWordCoordinates(wordTwo.getWord())) - pmiTable.getPPMI(wordOne.getWord(), wordTwo.getWord());
+                    }
+                    else
+                    {
+                        differences[wordOneIndex][wordTwoIndex] = DotProduct.compute(words.getWordCoordinates(wordOne.getWord()), contexts.getWordCoordinates(wordTwo.getWord())) - pmiTable.getPMI(wordOne.getWord(), wordTwo.getWord());
+                    }
+                    wordTwoIndex++;
                 }
-                else
-                {
-                    differences[wordOneIndex][wordTwoIndex] = DotProduct.compute(words.getWordCoordinates(wordOne.getWord()), contexts.getWordCoordinates(wordTwo.getWord())) - pmiTable.getPMI(wordOne.getWord(), wordTwo.getWord());
-                }
-                wordTwoIndex++;
+                wordOneIndex++;
             }
-            wordOneIndex++;
+            System.out.println();
         }
-        System.out.println();
         // Compute statistics
-        if ( arguments.getMin() )
+        if ( arguments.getWord2Vec() )
         {
-            min[0] = Min.compute(vocabulary, words, contexts);
-            min[1] = Min.compute(vocabulary, pmiTable, arguments.getPPMI());
-            min[2] = Min.compute(differences);
-            System.out.println("Minimum Word2Vec value: " + min[0]);
-            System.out.println("Minimum PMI value: " + min[1]);
-            System.out.println("Minimum difference: " + min[2]);
-            System.out.println();
+            if ( arguments.getMin() )
+            {
+                min[0] = Min.compute(vocabulary, words, contexts);
+                System.out.println("Minimum Word2Vec value: " + min[0]);
+            }
+            if ( arguments.getMean() )
+            {
+                mean[0] = Mean.compute(vocabulary, words, contexts);
+                System.out.println("Mean Word2Vec value: " + mean[0]);
+            }
+            if ( arguments.getStandardDeviation() )
+            {
+                standardDeviation[0] = StandardDeviation.compute(vocabulary, words, contexts);
+                System.out.println("Standard deviation Word2Vec value: " + standardDeviation[0]);
+            }
+            if ( arguments.getMax() )
+            {
+                max[0] = Max.compute(vocabulary, words, contexts);
+                System.out.println("Maximum Word2Vec value: " + max[0]);
+            }
+            if ( arguments.getHistogram() && (arguments.getMin() && arguments.getMax()) )
+            {
+                histogram[0] = Histogram.compute(vocabulary, words, contexts, min[0], max[0]);
+                System.out.println("\nHistogram of Word2Vec table\n");
+                Histogram.print(histogram[0], min[0], max[0]);
+                System.out.println();
+            }
         }
-        if ( arguments.getMean() )
+        if ( arguments.getPMI() || arguments.getPPMI() )
         {
-            mean[0] = Mean.compute(vocabulary, words, contexts);
-            mean[1] = Mean.compute(vocabulary, pmiTable, arguments.getPPMI());
-            mean[2] = Mean.compute(differences);
-            System.out.println("Mean Word2Vec value: " + mean[0]);
-            System.out.println("Mean PMI value: " + mean[1]);
-            System.out.println("Mean difference: " + mean[2]);
-            System.out.println();
+            if ( arguments.getMin() )
+            {
+                min[1] = Min.compute(vocabulary, pmiTable, arguments.getPPMI());
+                System.out.println("Minimum PMI value: " + min[1]);
+            }
+            if ( arguments.getMean() )
+            {
+                mean[1] = Mean.compute(vocabulary, pmiTable, arguments.getPPMI());
+                System.out.println("Mean PMI value: " + mean[1]);
+            }
+            if ( arguments.getStandardDeviation() )
+            {
+                standardDeviation[1] = StandardDeviation.compute(vocabulary, pmiTable, arguments.getPPMI());
+                System.out.println("Standard deviation PMI value: " + standardDeviation[1]);
+            }
+            if ( arguments.getMax() )
+            {
+                max[1] = Max.compute(vocabulary, pmiTable, arguments.getPPMI());
+                System.out.println("Maximum PMI value: " + max[1]);
+            }
+            if ( arguments.getHistogram() && (arguments.getMin() && arguments.getMax()) )
+            {
+                histogram[1] = Histogram.compute(vocabulary, pmiTable, min[1], max[1], arguments.getPPMI());
+                System.out.println("\nHistogram of PMI table\n");
+                Histogram.print(histogram[1], min[1], max[1]);
+                System.out.println();
+            }
         }
-        if ( arguments.getStandardDeviation() )
+        if ( arguments.getWord2Vec() && (arguments.getPMI() || arguments.getPPMI()) && arguments.getDifference() )
         {
-            standardDeviation[0] = StandardDeviation.compute(vocabulary, words, contexts);
-            standardDeviation[1] = StandardDeviation.compute(vocabulary, pmiTable, arguments.getPPMI());
-            standardDeviation[2] = StandardDeviation.compute(differences);
-            System.out.println("Standard deviation Word2Vec value: " + standardDeviation[0]);
-            System.out.println("Standard deviation PMI value: " + standardDeviation[1]);
-            System.out.println("Standard deviation of differences: " + standardDeviation[2]);
-            System.out.println();
-        }
-        if ( arguments.getMax() )
-        {
-            max[0] = Max.compute(vocabulary, words, contexts);
-            max[1] = Max.compute(vocabulary, pmiTable, arguments.getPPMI());
-            max[2] = Max.compute(differences);
-            System.out.println("Maximum Word2Vec value: " + max[0]);
-            System.out.println("Maximum PMI value: " + max[1]);
-            System.out.println("Maximum difference: " + max[2]);
-            System.out.println();
-        }
-        if ( arguments.getHistogram() && (arguments.getMin() && arguments.getMax()) )
-        {
-            histogram = Histogram.compute(differences, min[2], max[2]);
-            System.out.println("\nHistogram of differences\n");
-            Histogram.print(histogram, min[2], max[2]);
-            System.out.println();
+            if ( arguments.getMin() )
+            {
+                min[2] = Min.compute(differences);
+                System.out.println("Minimum difference: " + min[2]);
+            }
+            if ( arguments.getMean() )
+            {
+                mean[2] = Mean.compute(differences);
+                System.out.println("Mean difference: " + mean[2]);
+            }
+            if ( arguments.getStandardDeviation() )
+            {
+                standardDeviation[2] = StandardDeviation.compute(differences);
+                System.out.println("Standard deviation of differences: " + standardDeviation[2]);
+            }
+            if ( arguments.getMax() )
+            {
+                max[2] = Max.compute(differences);
+                System.out.println("Maximum difference: " + max[2]);
+            }
+            if ( arguments.getHistogram() && (arguments.getMin() && arguments.getMax()) )
+            {
+                histogram[2] = Histogram.compute(differences, min[2], max[2]);
+                System.out.println("\nHistogram of differences\n");
+                Histogram.print(histogram[2], min[2], max[2]);
+                System.out.println();
+            }
         }
         if ( arguments.getSpearman() )
         {
