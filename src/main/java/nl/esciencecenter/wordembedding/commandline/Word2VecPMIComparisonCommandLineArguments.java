5625ac857edc568714f052ef57573a32a9aa6dea
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2018-11-16 13:53:54 +0100

    Add threshold on word occurrences in the vocabulary.

--- src/main/java/nl/esciencecenter/wordembedding/commandline/Word2VecPMIComparisonCommandLineArguments.java
+++ src/main/java/nl/esciencecenter/wordembedding/commandline/Word2VecPMIComparisonCommandLineArguments.java
@@ -18,6 +18,8 @@ public class Word2VecPMIComparisonCommandLineArguments
     private String vocabularyFilename = "";
     @Parameter(names = {"-window"}, description = "Window size for pair generation.")
     private int window = 2;
+    @Parameter(names = {"-max_occurrences"}, description = "Threshold for the vocabulary.")
+    private int maxOccurrences = 0;
     @Parameter(names = {"-corpus_file"}, description = "Corpus file.")
     private String corpusFilename = "";
     @Parameter(names = {"-vector_file"}, description = "Word2Vec vector file.")
@@ -71,6 +73,11 @@ public class Word2VecPMIComparisonCommandLineArguments
     public int getWindow()
     {
         return window;
+    }
+
+    public int getMaxOccurrences()
+    {
+        return maxOccurrences;
     }
 
     public String getCorpusFileName()
