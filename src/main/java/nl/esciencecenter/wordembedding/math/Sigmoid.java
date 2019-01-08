59fc300f2f67cb1d5c9fdda697bb75e48fd788c6
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-01-08 11:49:58 +0100

    Added sigmoid function.

---
+++ src/main/java/nl/esciencecenter/wordembedding/math/Sigmoid.java
@@ -0,0 +1,9 @@
+package nl.esciencecenter.wordembedding.math;
+
+import static java.lang.Math.exp;
+
+public class Sigmoid {
+    public static double compute(double x) {
+        return 1 / (1 - Math.exp(-x));
+    }
+}
