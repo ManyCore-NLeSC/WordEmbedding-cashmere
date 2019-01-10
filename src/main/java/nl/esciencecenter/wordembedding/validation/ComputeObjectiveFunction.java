231eb0bceaa3ae2b36575eba60d969376b8ba3c0
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-01-10 15:44:43 +0100

    Experiment to compute the deviation between optimum and word2vec.

--- src/main/java/nl/esciencecenter/wordembedding/validation/ComputeObjectiveFunction.java
+++ src/main/java/nl/esciencecenter/wordembedding/validation/ComputeObjectiveFunction.java
@@ -1,15 +1,12 @@
 package nl.esciencecenter.wordembedding.validation;
 
-import nl.esciencecenter.wordembedding.data.Vocabulary;
-import nl.esciencecenter.wordembedding.data.Word;
-import nl.esciencecenter.wordembedding.data.WordEmbedding;
-import nl.esciencecenter.wordembedding.data.WordPairs;
+import nl.esciencecenter.wordembedding.data.*;
 import nl.esciencecenter.wordembedding.math.DotProduct;
 import nl.esciencecenter.wordembedding.math.Negate;
 import nl.esciencecenter.wordembedding.math.Sigmoid;
 
 public class ComputeObjectiveFunction {
-    public static double compute(Vocabulary vocabulary, WordPairs pairs, WordEmbedding words, WordEmbedding contexts, int k)
+    public static double objectiveFunctionWord2Vec(Vocabulary vocabulary, WordPairs pairs, WordEmbedding words, WordEmbedding contexts, int k)
     {
         double globalObjective = 0;
         for ( Word wordOne : vocabulary.getWords() )
@@ -31,5 +28,34 @@ public class ComputeObjectiveFunction {
             }
         }
         return globalObjective;
+    }
+
+    public static double deviationFromOptimalWord2Vec(Vocabulary vocabulary, WordPairs pairs, PMITable pmiTable, WordEmbedding words, WordEmbedding contexts, int k)
+    {
+        double word2vecObjective = 0;
+        double pmiObjective = 0;
+        for ( Word wordOne : vocabulary.getWords() )
+        {
+            if ( wordOne.getWord().equals("</s>") )
+            {
+                continue;
+            }
+            for ( Word wordTwo : vocabulary.getWords() )
+            {
+                if ( wordTwo.getWord().equals("</s>") )
+                {
+                    continue;
+                }
+                double localObjective = pairs.getPairOccurrences(wordOne.getWord(), wordTwo.getWord());
+                localObjective *= Math.log(Sigmoid.compute(DotProduct.compute(words.getWordCoordinates(wordOne.getWord()), contexts.getWordCoordinates(wordTwo.getWord()))));
+                localObjective += k * pairs.getOccurrences(wordOne.getWord()) * ((float)(pairs.getOccurrences(wordTwo.getWord())) / (float)(pairs.getTotalPairs())) * Math.log(Sigmoid.compute(DotProduct.compute(Negate.compute(words.getWordCoordinates(wordOne.getWord())), contexts.getWordCoordinates(wordTwo.getWord()))));
+                word2vecObjective += localObjective;
+                localObjective = pairs.getPairOccurrences(wordOne.getWord(), wordTwo.getWord());
+                localObjective *= Math.log(Sigmoid.compute(pmiTable.getPMI(wordOne.getWord(), wordTwo.getWord()) - Math.log(k)));
+                localObjective += k * pairs.getOccurrences(wordOne.getWord()) * ((float)(pairs.getOccurrences(wordTwo.getWord())) / (float)(pairs.getTotalPairs())) * Math.log(Sigmoid.compute(pmiTable.getPMI(wordOne.getWord(), wordTwo.getWord()) - Math.log(k)));
+                pmiObjective += localObjective;
+            }
+        }
+        return (word2vecObjective - pmiObjective) / pmiObjective;
     }
 }
