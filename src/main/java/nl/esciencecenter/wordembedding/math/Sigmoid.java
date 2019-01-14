2a95310bc1673d6484adab2ef212942b84106a8c
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-01-14 11:42:16 +0100

    Sign error in the sigmoid function.

--- src/main/java/nl/esciencecenter/wordembedding/math/Sigmoid.java
+++ src/main/java/nl/esciencecenter/wordembedding/math/Sigmoid.java
@@ -4,6 +4,6 @@ import static java.lang.Math.exp;
 
 public class Sigmoid {
     public static double compute(double x) {
-        return 1 / (1 - Math.exp(-x));
+        return 1 / (1 + Math.exp(-x));
     }
 }
