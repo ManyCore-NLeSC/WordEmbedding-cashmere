6a8c1d02199b146dec7eeffc9a0bdce46f75e800
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-01-15 16:05:14 +0100

    Remove unused variables.

--- src/main/java/nl/esciencecenter/wordembedding/commandline/Word2VecPMIComparisonCommandLineArguments.java
+++ src/main/java/nl/esciencecenter/wordembedding/commandline/Word2VecPMIComparisonCommandLineArguments.java
@@ -8,37 +8,19 @@ public class Word2VecPMIComparisonCommandLineArguments
     private boolean help = false;
     @Parameter(names = {"-ppmi"}, description = "Use PPMI instead of PMI.")
     private boolean ppmi = false;
-    @Parameter(names = {"-vocabulary_file"}, description = "Word2Vec vocabulary file.", required = true)
-    private String vocabularyFilename = "";
     @Parameter(names = {"-window"}, description = "Window size for pair generation.")
     private int window = 2;
     @Parameter(names = {"-negative_samples"}, description = "Negative samples used for Word2Vec.")
     private int negativeSamples = 1;
+    // Files
+    @Parameter(names = {"-vocabulary_file"}, description = "Word2Vec vocabulary file.", required = true)
+    private String vocabularyFilename = "";
     @Parameter(names = {"-corpus_file"}, description = "Corpus file.")
     private String corpusFilename = "";
     @Parameter(names = {"-vector_file"}, description = "Word2Vec vector file.")
     private String vectorFilename = "";
     @Parameter(names = {"-context_file"}, description = "Word2Vec context file.")
     private String contextFilename = "";
-    // Statistics
-    @Parameter(names = {"-max"}, description = "Compute the maximum difference.")
-    private boolean max = false;
-    @Parameter(names = {"-min"}, description = "Compute the minimum difference.")
-    private boolean min = false;
-    @Parameter(names = {"-mean"}, description = "Compute the mean difference.")
-    private boolean mean = false;
-    @Parameter(names = {"-std"}, description = "Compute the standard deviation of differences.")
-    private boolean std = false;
-    @Parameter(names = {"-histogram"}, description = "Compute the histogram of differences distribution.")
-    private boolean histogram = false;
-    @Parameter(names = {"-histogram_size"}, description = "Number of bins in the histogram.")
-    private int histogramSize = 100;
-    @Parameter(names = {"-spearman"}, description = "Compute the Spearman correlation between Word2Vec and PMI/PPMI.")
-    private boolean spearman = false;
-    @Parameter(names = {"-noinf"}, description = "Compute values from Word2Vec only if they are finite in PMI.")
-    private boolean noInf = false;
-    @Parameter(names = {"-objective_functions"}, description = "Compute values of the objective functions.")
-    private boolean objectives = false;
 
     public boolean getHelp()
     {
@@ -78,50 +60,5 @@ public class Word2VecPMIComparisonCommandLineArguments
     public String getContextFileName()
     {
         return contextFilename;
-    }
-
-    public boolean getMax()
-    {
-        return max;
-    }
-
-    public boolean getMin()
-    {
-        return min;
-    }
-
-    public boolean getMean()
-    {
-        return mean;
-    }
-
-    public boolean getStandardDeviation()
-    {
-        return std;
-    }
-
-    public boolean getHistogram()
-    {
-        return histogram;
-    }
-
-    public int getHistogramSize()
-    {
-        return histogramSize;
-    }
-
-    public boolean getSpearman()
-    {
-        return spearman;
-    }
-
-    public boolean getNoInf()
-    {
-        return noInf;
-    }
-
-    public boolean getObjective()
-    {
-        return objectives;
     }
 }
