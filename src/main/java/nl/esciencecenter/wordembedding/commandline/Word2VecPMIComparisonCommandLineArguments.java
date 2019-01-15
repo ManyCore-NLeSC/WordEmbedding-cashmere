79b37b4bbe67fb38b827cd3a9904bfffca614add
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-01-15 16:09:59 +0100

    Added a command line parameter for sampling of the vocabulary.

--- src/main/java/nl/esciencecenter/wordembedding/commandline/Word2VecPMIComparisonCommandLineArguments.java
+++ src/main/java/nl/esciencecenter/wordembedding/commandline/Word2VecPMIComparisonCommandLineArguments.java
@@ -12,14 +12,16 @@ public class Word2VecPMIComparisonCommandLineArguments
     private int window = 2;
     @Parameter(names = {"-negative_samples"}, description = "Negative samples used for Word2Vec.")
     private int negativeSamples = 1;
+    @Parameter(names = {"-sampling_rate"}, description = "Probability that a word is removed from the vocabulary.")
+    private int samplingRate = 0;
     // Files
     @Parameter(names = {"-vocabulary_file"}, description = "Word2Vec vocabulary file.", required = true)
     private String vocabularyFilename = "";
-    @Parameter(names = {"-corpus_file"}, description = "Corpus file.")
+    @Parameter(names = {"-corpus_file"}, description = "Corpus file.", required = true)
     private String corpusFilename = "";
-    @Parameter(names = {"-vector_file"}, description = "Word2Vec vector file.")
+    @Parameter(names = {"-vector_file"}, description = "Word2Vec vector file.", required = true)
     private String vectorFilename = "";
-    @Parameter(names = {"-context_file"}, description = "Word2Vec context file.")
+    @Parameter(names = {"-context_file"}, description = "Word2Vec context file.", required = true)
     private String contextFilename = "";
 
     public boolean getHelp()
@@ -45,6 +47,11 @@ public class Word2VecPMIComparisonCommandLineArguments
     public int getNegativeSamples()
     {
         return negativeSamples;
+    }
+
+    public int getSamplingRate()
+    {
+        return samplingRate;
     }
 
     public String getCorpusFileName()
