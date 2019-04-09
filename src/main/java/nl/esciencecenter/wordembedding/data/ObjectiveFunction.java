0e989fcf04a5d84c4497d634a6deb4ed8759a3be
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-04-09 13:51:05 +0200

    Objective function (Eq. 1).

---
+++ src/main/java/nl/esciencecenter/wordembedding/data/ObjectiveFunction.java
@@ -0,0 +1,33 @@
+package nl.esciencecenter.wordembedding.data;
+
+import nl.esciencecenter.wordembedding.math.DotProduct;
+import nl.esciencecenter.wordembedding.math.Sigmoid;
+
+public class ObjectiveFunction {
+    private double pmi = 0;
+    private double word2vec = 0;
+
+    public double getPMI()
+    {
+        return pmi;
+    }
+
+    public double getWord2Vec()
+    {
+        return word2vec;
+    }
+
+    public void incrementPMI(PMITable pmiTable, String wordOne, String wordTwo, int k)
+    {
+        double localObjective = Math.log(Sigmoid.compute(pmiTable.getPMI(wordOne, wordTwo) - Math.log(k)));
+        localObjective += k * Math.log(Sigmoid.compute(-(pmiTable.getPMI(wordOne, wordTwo) - Math.log(k))));
+        pmi += localObjective;
+    }
+
+    public void incrementWord2Vec(WordEmbedding words, WordEmbedding contexts, String wordOne, String wordTwo, int k)
+    {
+        double localObjective = Math.log(Sigmoid.compute(DotProduct.compute(words.getWordCoordinates(wordOne), contexts.getWordCoordinates(wordTwo))));
+        localObjective += k * Math.log(Sigmoid.compute(-(DotProduct.compute(words.getWordCoordinates(wordOne), contexts.getWordCoordinates(wordTwo)))));
+        word2vec += localObjective;
+    }
+}
---
+++ src/main/java/nl/esciencecenter/wordembedding/utilities/ComputeObjectiveFunction.java
@@ -0,0 +1,34 @@
+package nl.esciencecenter.wordembedding.utilities;
+
+import nl.esciencecenter.wordembedding.data.ObjectiveFunction;
+import nl.esciencecenter.wordembedding.data.PMITable;
+import nl.esciencecenter.wordembedding.data.Vocabulary;
+import nl.esciencecenter.wordembedding.data.WordEmbedding;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ComputeObjectiveFunction {
+    public static void compute(ObjectiveFunction function, Vocabulary vocabulary, PMITable pmiTable, WordEmbedding words, WordEmbedding contexts, int k, BufferedReader fileReader) throws IOException
+    {
+        String line;
+
+        while ( (line = fileReader.readLine()) != null ) {
+            String [] values = line.split("[ \t]+");
+            for ( int word = 0; word < values.length; word++ )
+            {
+                for ( int targetWord = word - pmiTable.getPairs().getWindowSize(); targetWord <= word + pmiTable.getPairs().getWindowSize(); targetWord++ )
+                {
+                    if ( (word != targetWord) && (targetWord >= 0) && (targetWord < values.length) )
+                    {
+                        if ( (vocabulary.getWord(values[word]) != null) && (vocabulary.getWord(values[targetWord]) != null) )
+                        {
+                            function.incrementPMI(pmiTable, values[word], values[targetWord], k);
+                            function.incrementWord2Vec(words, contexts, values[word], values[targetWord], k);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
