d9022e6a8d82956fc29f6f8596965fc0377b5f52
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2019-04-23 11:56:02 +0200

    Using random words as context words for negative sampling.

--- src/main/java/nl/esciencecenter/wordembedding/data/ObjectiveFunction.java
+++ src/main/java/nl/esciencecenter/wordembedding/data/ObjectiveFunction.java
@@ -29,10 +29,24 @@ public class ObjectiveFunction {
         pmi += localObjective;
     }
 
+    public void incrementPMI(PMITable pmiTable, Vocabulary vocabulary, String wordOne, String wordTwo, int k)
+    {
+        double localObjective = Math.log(Sigmoid.compute(pmiTable.getPMI(wordOne, wordTwo) - Math.log(k)));
+        localObjective += k * Math.log(Sigmoid.compute(-(pmiTable.getPMI(wordOne, vocabulary.getRandomWord()) - Math.log(k))));
+        pmi += localObjective;
+    }
+
     public void incrementWord2Vec(WordEmbedding words, WordEmbedding contexts, String wordOne, String wordTwo, int k)
     {
         double localObjective = Math.log(Sigmoid.compute(DotProduct.compute(words.getWordCoordinates(wordOne), contexts.getWordCoordinates(wordTwo))));
         localObjective += k * Math.log(Sigmoid.compute(-(DotProduct.compute(words.getWordCoordinates(wordOne), contexts.getWordCoordinates(wordTwo)))));
+        word2vec += localObjective;
+    }
+
+    public void incrementWord2Vec(Vocabulary vocabulary, WordEmbedding words, WordEmbedding contexts, String wordOne, String wordTwo, int k)
+    {
+        double localObjective = Math.log(Sigmoid.compute(DotProduct.compute(words.getWordCoordinates(wordOne), contexts.getWordCoordinates(wordTwo))));
+        localObjective += k * Math.log(Sigmoid.compute(-(DotProduct.compute(words.getWordCoordinates(wordOne), contexts.getWordCoordinates(vocabulary.getRandomWord())))));
         word2vec += localObjective;
     }
 }
